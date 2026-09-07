60d7c23108bc2f51142d4d11e1452fbc79fb20e6
pablogarciablancov <pgarciab@fomento.edu>
2026-09-07 19:41:50 +0200

    perf: versiona batalla limpia v10

--- games/battlegrafia_v2/battle-clean-v2.js
+++ games/battlegrafia_v2/battle-clean-v2.js
@@ -8,7 +8,7 @@
     const link=document.createElement('link');
     link.id='bg2-clean-battle-css';
     link.rel='stylesheet';
-    link.href='./battle-clean-v2.css?v=20260907-rpg9';
+    link.href='./battle-clean-v2.css?v=20260907-rpg10';
     document.head.appendChild(link);
   }
 
