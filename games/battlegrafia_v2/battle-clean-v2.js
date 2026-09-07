80eec1af16f952b560883494d47533bf2cd8e4a4
pablogarciablancov <pgarciab@fomento.edu>
2026-09-07 19:31:03 +0200

    Battlegrafía 2.0: versiona la nueva batalla limpia

--- games/battlegrafia_v2/battle-clean-v2.js
+++ games/battlegrafia_v2/battle-clean-v2.js
@@ -8,7 +8,7 @@
     const link=document.createElement('link');
     link.id='bg2-clean-battle-css';
     link.rel='stylesheet';
-    link.href='./battle-clean-v2.css?v=20260907-rpg8';
+    link.href='./battle-clean-v2.css?v=20260907-rpg9';
     document.head.appendChild(link);
   }
 
