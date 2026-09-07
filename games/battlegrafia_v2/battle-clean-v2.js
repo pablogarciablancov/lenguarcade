fe6ef065f8c0599685197cd13f7e3e1c23f835ca
pablogarciablancov <pgarciab@fomento.edu>
2026-09-07 20:27:15 +0200

    Renueva cache de estilos de nombres rpg16

--- games/battlegrafia_v2/battle-clean-v2.js
+++ games/battlegrafia_v2/battle-clean-v2.js
@@ -8,7 +8,7 @@
     const link=document.createElement('link');
     link.id='bg2-clean-battle-css';
     link.rel='stylesheet';
-    link.href='./battle-clean-v2.css?v=20260907-rpg15';
+    link.href='./battle-clean-v2.css?v=20260907-rpg16';
     document.head.appendChild(link);
   }
 
