edba9b164b7f629dc2420b0c04d907304f0b8c3f
pablogarciablancov <pgarciab@fomento.edu>
2026-09-07 20:43:27 +0200

    Renueva cache del HUD corregido rpg19

--- games/battlegrafia_v2/battle-clean-v2.js
+++ games/battlegrafia_v2/battle-clean-v2.js
@@ -8,7 +8,7 @@
     const link=document.createElement('link');
     link.id='bg2-clean-battle-css';
     link.rel='stylesheet';
-    link.href='./battle-clean-v2.css?v=20260907-rpg17';
+    link.href='./battle-clean-v2.css?v=20260907-rpg19';
     document.head.appendChild(link);
   }
 
