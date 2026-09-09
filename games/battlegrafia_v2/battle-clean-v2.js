2872e91fbd3153893b44e2e70d1320651812f075
pablogarciablancov <pgarciab@fomento.edu>
2026-09-10 00:19:48 +0200

    Battlegrafía: refresca CSS de combate

--- games/battlegrafia_v2/battle-clean-v2.js
+++ games/battlegrafia_v2/battle-clean-v2.js
@@ -8,7 +8,7 @@
     const link=document.createElement('link');
     link.id='bg2-clean-battle-css';
     link.rel='stylesheet';
-    link.href='./battle-clean-v2.css?v=20260907-rpg19';
+    link.href='./battle-clean-v2.css?v=20260910-rpg31';
     document.head.appendChild(link);
   }
 
